f804f1aa7eb8158060ceac2bb3ec32c19995b8f7
不二 <1104891151@qq.com>
2020-07-03 00:23:46 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -18,13 +18,13 @@ def start():
     referer = 'https://glados.rocks/console/checkin'
     checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
     state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
-   # print(res)
+    #print(res)
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
-        #print(time)
+        print(“剩余天数：”+time)
         if sever == 'on':
             requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
     else:
