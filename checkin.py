5ea6a6532e9576380160b59163ceb6bd04ebbde0
不二 <1104891151@qq.com>
2020-07-03 00:26:19 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -24,7 +24,7 @@ def start():
         mess = checkin.json()['message']
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
-        print(“剩余天数：”+time)
+        print("剩余天数："+time)
         if sever == 'on':
             requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
     else:
